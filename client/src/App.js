d2c3dbc79c4e1c032c7b90d9393ab9dc96e29f34
juarezfrench <markedfrench@gmail.com>
2019-10-22 16:35:11 -0500

    "Git gui."

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React, {  useReducer } from 'react';
+import './App.css';
+
+import { initialState, todoReducer } from './components/reducers/todoReducer';
+import FormikLoginForm from './components/loginPage/Form'
+
+// import TodoList from './components/TodoList';
+// import TodoForm from './components/TodoForm';
+// import TodoClear from './components/TodoClear';
+
+function App() {
+  const [state, dispatch] = useReducer(todoReducer, initialState);
+
+  return (
+    <div className='App'>
+      {/* <h1>Reducer Todo App</h1>
+      <TodoForm dispatch={dispatch} />
+      <TodoList state={state} dispatch={dispatch} />
+      <TodoClear dispatch={dispatch} /> */}
+
+    <FormikLoginForm/>
+
+    </div>
+  );
+}
+
+export default App;
