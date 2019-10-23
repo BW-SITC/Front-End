e8cdbffb4a1e01499462acbcfaf4e4fb3670b609
juarezfrench <markedfrench@gmail.com>
2019-10-23 16:07:49 -0500

    Fixed issue with Volunteer list

--- client/src/components/VolunteerList.js
+++ client/src/components/VolunteerList.js
@@ -1,17 +1,17 @@
-import React,{useState} from 'react'
+import React,{useState, useEffect} from 'react'
 import axios from 'axios'
 
 const VolunteerList  = () => {
 
 const [volunteers, setVolunteers] = useState([])    
-    
+    useEffect(() => {
     axios
     .get(`https://school-in-cloud.herokuapp.com/api/student/`)
-    .then(res => setVolunteers( res.data) );
-    .catch(err => console.error("Good data's gone bad here:",err.response));
+    .then(res => setVolunteers( res.data) )
+    .catch(err => console.error("Good data's gone bad here:",err.response))
     
-    console.log('get response', res);
-
+    // console.log('get response', res);
+},[])
 return (
 <div>
   {volunteers.map(volunteer=>{
